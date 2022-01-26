da99ff6e85d229c1e8fc8464b26c947e5815767b
于紫萱 <yuzixuancn@qq.com>
2022-01-26 22:01:13 +0800

    增强猜数游戏

--- 猜数游戏.py
+++ 猜数游戏.py
@@ -5,19 +5,19 @@ import random
 secret = random.randint(1, 99)
 guess = 0
 tries = 0
-print('我是一个海盗, 我有一个秘密')
-print('有一个数字在1-99之间, 我让你猜6次')
-while guess != secret and tries < 6:
+print('我是爱心, 我们来玩猜数游戏吧')
+print('有一个数字在1-100之间, 我让你猜很多次')
+while guess != secret and tries < 600:
     guess = int(input('你猜的数字是什么: '))
     if guess < secret:
         print('数字太小')
     elif guess > secret:
         print('数字太大')
-    
+
     tries = tries + 1
 
 if guess == secret:
-    print('啊, 你发现了我的秘密')
+    print('你猜对了, 恭喜你')
 else:
     print('你没有机会再猜了, 等下次吧')
     print('秘密数字是: ', secret)
